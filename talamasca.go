b6008ab82131c63bed6dd98997d5d84019412c43
Chris Zietlow <aczietlow@gmail.com>
2023-10-12 22:30:23 -0400

    Added config.json

--- talamasca.go
+++ talamasca.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/google/go-github/v53/github"
+	"os"
 )
+
+type Config struct {
+	Repo  string `json:"repo"`
+	Owner string `json:"owner"`
+}
 
 // Fetch all releases for a given repository
 func fetchRepoReleases(owner string, reponame string) ([]*github.RepositoryRelease, error) {
@@ -14,11 +21,27 @@ func fetchRepoReleases(owner string, reponame string) ([]*github.RepositoryRelea
 	return releases, err
 }
 
-func main() {
-	owner := "pantheon-systems"
-	repo := "search_api_pantheon"
+func loadConfig(filename string) (*Config, error) {
+	configFile, err := os.ReadFile(filename)
 
-	releases, err := fetchRepoReleases(owner, repo)
+	if err != nil {
+		return nil, err
+	}
+	config := &Config{}
+	err = json.Unmarshal(configFile, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func main() {
+
+	// @TODO deal with errors later when I actually care.
+	config, _ := loadConfig("./config.json")
+
+	releases, err := fetchRepoReleases(config.Owner, config.Repo)
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
 		return
@@ -47,14 +70,14 @@ func main() {
 	//}
 
 	// Get repository.
-	commits, _, err := client.Repositories.CompareCommits(context.Background(), owner, repo, last, current, nil)
+	commits, _, err := client.Repositories.CompareCommits(context.Background(), config.Owner, config.Repo, last, current, nil)
 
 	// Print commits
 	for i, commit := range commits.Commits {
 		fmt.Printf("%v. %v - %v\n", i+1, commit.GetSHA(), commit.GetCommit().GetMessage())
 	}
 
-	commit, _, err := client.Repositories.GetCommit(context.Background(), owner, repo, "aa88ed97ad7270d83bc3425fbe9bbe401c7c41f4", nil)
+	commit, _, err := client.Repositories.GetCommit(context.Background(), config.Owner, config.Repo, "aa88ed97ad7270d83bc3425fbe9bbe401c7c41f4", nil)
 
 	ghAuthor := commit.GetAuthor()
 	fmt.Printf("Github User: Name-%v\nID-%v\nEmail-%v\n", ghAuthor.GetLogin(), commit.GetAuthor().GetID(), commit.GetAuthor().GetEmail())
